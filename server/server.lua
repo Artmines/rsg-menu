a93467b82323bca41272fa2fb93054e375b55e43
Artmines <96462463+Artmines@users.noreply.github.com>
2024-02-18 09:59:06 -0700

    Update server version check

--- server/server.lua
+++ server/server.lua
@@ -10,7 +10,7 @@ local function versionCheckPrint(_type, log)
 end
 
 local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-menu/main/version.txt', function(err, text, headers)
+    PerformHttpRequest('https://raw.githubusercontent.com/Artmines/rsg-menu/main/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
         if not text then 
